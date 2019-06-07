654d5aa7481d49d0883f9be0b45b733ac9012e87
Bartłomiej Olszanowski <baloo@int.pl>
2019-06-07 15:55:18 +0200

    Added HC06

--- 2019_Gimball.ino
+++ 2019_Gimball.ino
@@ -1,42 +1,99 @@
 HardwareSerial Serial1(PA10, PA9);
+const int led=2;
+int speedP=0;
+int speedL=0;
+int direction=5;
 
-char cmd[5];
-int cmdIndex = 0;
+//Motor A
+int PWMA = 3; //Speed control
+int AIN1 = 9; //Direction
+int AIN2 = 8; //Direction
+
+//Motor B
+int PWMB = 5; //Speed control
+int BIN1 = 11; //Direction
+int BIN2 = 12; //Direction
 
 void setup() {
   Serial.begin(9600); //ST-LINK Virtual COM Port
   Serial1.begin(9600); // UART1
+
+  pinMode(STBY, OUTPUT);
+
+  pinMode(PWMA, OUTPUT);
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+
+  pinMode(PWMB, OUTPUT);  
+  pinMode(BIN1, OUTPUT);
+  pinMode(BIN2, OUTPUT);
   
   pinMode(LED_BUILTIN,OUTPUT);
 
 }
 
-void exeCmd() {
-  Serial.println("CP2");
-  Serial.println(cmd);
-  if(strcmp(cmd, "led 1")==0){
-    digitalWrite(LED_BUILTIN,HIGH);
-    delay(1000);
-  } 
-  if(strcmp(cmd, "led 0")==0){
-    digitalWrite(LED_BUILTIN,LOW);
-    delay(1000);
+void loop() {
+  if(Serial1.available())
+  {
+    char a=Serial1.read();
+    if(a=='1'){
+      speedP=speedP+10;
+      speedL=speedL+10;
+    }
+    else if(a=='2'){
+      speedP=speedP-10;
+      speedL=speedL-10;      
+    }
+    else if(a=='3'){
+      speedP=speedP+10;
+    }
+    else if(a=='4'){
+      speedL=speedL+10;
+    }
+    else if(a=='5'){
+     speedP=speedL;
+    }
+    else if(a=='6'){
+      speedL=speedP;
+    }
+    else if(a=='7'){
+      digitalWrite(LED_BUILTIN,HIGH);
+    }
+    else if(a=='8'){
+      digitalWrite(LED_BUILTIN, LOW);
+    }
   }
- 
+  move(1,speedP);
+  move(2,speedL);
 }
 
-void loop() {
-  if (Serial1.available())
-  {
-    Serial.println("CP1");
-    char c = (char)Serial1.read();
-    if (c == '\n') {
-      cmd[cmdIndex] = 0;
-      exeCmd();
-      cmdIndex = 0;
-    } else {
-      cmd[cmdIndex] = c;
-      if (cmdIndex < 5) cmdIndex++;
-    }
-  } 
+void move(int motor, int speed){
+  //Move specific motor at speed and direction
+  //motor: 0 for B 1 for A
+  //speed: 0 is off, and 255 is full speed
+  //direction: 0 clockwise, 1 counter-clockwise
+  digitalWrite(STBY, HIGH); //disable standby
+
+  boolean inPin1 = LOW;
+  boolean inPin2 = HIGH;
+  
+  if(speed>0){
+    direction=1;
+  } else{
+    direction=0;
+  }
+  if(direction==1){
+    inPin1=HIGH;
+    inPin2=LOW;
+  }
+  if(motor == 1){
+    digitalWrite(AIN1, inPin1);
+    digitalWrite(AIN2, inPin2);
+    analogWrite(PWMA, speed);
+   }else{
+    digitalWrite(BIN1, inPin1);
+    digitalWrite(BIN2, inPin2);
+    analogWrite(PWMB, speed);
+  }
+
 }
